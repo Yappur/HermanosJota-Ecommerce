1786bbf3e34a1954100e50cdf1783e0af4377fcd
unknown <mateox463@gmail.com>
2025-09-30 17:10:18 -0300

    Se elimina carpeta pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
+import NavBar from "./components/Navbar";
 function App() {
-   const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-      fetch("http://localhost:3001/api/hello")
-        .then((res) => res.json())
-        .then((data) => setMessage(data.message))
-        .catch((err) => console.error(err));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:3001/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <>
       <div>
