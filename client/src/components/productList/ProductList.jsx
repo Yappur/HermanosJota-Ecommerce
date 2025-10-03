0d652d851e4e40445c0d31082a229594c8d4a5e0
Emmanuel Martínez <emmanuel1407@hotmail.com>
2025-10-03 09:45:24 -0300

    Se agrega el import del archivo css a ProductItem

--- client/src/components/productList/ProductList.jsx
+++ client/src/components/productList/ProductList.jsx
@@ -1,38 +1,38 @@
 import ProductItem from './productItem/ProductItem'
-// import { productos } from '../../api/data/products.js'
+import { productos } from '../../api/data/products.js'
 import { useEffect, useState } from 'react'
 import "./product-list.css"
 
 const ProductList = () => {
-    const [productos, setProductos] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
+    // const [productos, setProductos] = useState([])
+    // const [loading, setLoading] = useState(true)
+    // const [error, setError] = useState(null)
 
     // Fetch para traer los productos del backend
-    useEffect(() => {
-        const fetchProductos = async () => {
-        setLoading(true)
-        setError(null)
-        try {
-            const response = await fetch('http://localhost:3001/api/productos')
-            if (!response.ok) {
-            throw new Error('Error al traer productos')
-            }
-            const data = await response.json()
-            setProductos(data)
-        } catch (err) {
-            setError(err.message)
-        } finally {
-            setLoading(false)
-        }
-        }
+    // useEffect(() => {
+    //     const fetchProductos = async () => {
+    //     setLoading(true)
+    //     setError(null)
+    //     try {
+    //         const response = await fetch('http://localhost:3001/api/productos')
+    //         if (!response.ok) {
+    //         throw new Error('Error al traer productos')
+    //         }
+    //         const data = await response.json()
+    //         setProductos(data)
+    //     } catch (err) {
+    //         setError(err.message)
+    //     } finally {
+    //         setLoading(false)
+    //     }
+    //     }
 
-        fetchProductos()
-    }, [])
+    //     fetchProductos()
+    // }, [])
 
     return (
         <div className='products-container'>
-            {loading && <p>Cargando productos...</p>}
+            {/* {loading && <p>Cargando productos...</p>}
 
             {error &&
                 <div className='products-empty'>
@@ -42,11 +42,11 @@ const ProductList = () => {
                 </div>
             }
 
-            {!loading && !error && productos.length === 0 && <p>No hay productos.</p>}
+            {!loading && !error && productos.length === 0 && <p>No hay productos.</p>} */}
 
             {
-            !loading &&
-            !error &&
+            // !loading &&
+            // !error &&
             productos.map(producto => (
                 <ProductItem key={producto.id} producto={producto} />
             ))}
--- client/src/components/productList/productItem/ProductItem.jsx
+++ client/src/components/productList/productItem/ProductItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./product-item.css";
 
 
 const ProductItem = (props) => {
