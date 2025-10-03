0fd73b50b0c9d12dc83ac012cb22e7969aab5cf7
Emmanuel Martínez <emmanuel1407@hotmail.com>
2025-10-03 07:44:49 -0300

    Se agrega el fetch a productos en el backend

--- client/src/components/productList/productItem/ProductItem.jsx
+++ client/src/components/productList/productItem/ProductItem.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 
 const ProductItem = (props) => {
     const {
-        product,
+        producto,
     } = props;
 
   return (
     <article className='products-card'>
-        <a className='products-link' href={`./product.html?id=${encodeURIComponent(product.id)}`}>
-            <img className='products-image' src={`${product.image}`} alt={product.name} />
-            <h4 className='products-name'>{product.name}</h4>
+        <a className='products-link' href={`./product.html?id=${encodeURIComponent(producto.id)}`}>
+            <img className='products-image' src={`${producto.imagen}`} alt={producto.nombre} />
+            <h4 className='products-name'>{producto.nombre}</h4>
         </a>
-        <p className='products-description'>{product.description}</p>
-        <span className='products-price'>${Number(product.precio || 0).toLocaleString('es-AR')}</span>
+        <p className='products-description'>{producto.descripcion}</p>
+        <span className='products-price'>${Number(producto.precio || 0).toLocaleString('es-AR')}</span>
     </article>
   )
 }
