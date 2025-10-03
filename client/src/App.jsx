5d418b00282c69b9a609e4de3039e8d3d19600d3
Emmanuel Martínez <emmanuel1407@hotmail.com>
2025-10-03 08:40:18 -0300

    Arreglos mínimos en el front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import Index from "./components/Index";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/navbar/Navbar";
 import HeroSection from "./components/Hero/HeroSection";
-import Contact from "./components/Contact/contactForm";
 import About from "./components/About/About";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <HeroSection />
       <About />
       <Footer />
-      <ContactForm />
     </CartProvider>
   );
 }
