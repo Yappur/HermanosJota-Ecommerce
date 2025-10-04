f9dc32f218535922444e82714140e769d59199fb
unknown <mateox463@gmail.com>
2025-10-04 15:40:27 -0300

    Se elimina linea que traia error al deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { CartProvider } from "./context/CartContext";
-import Index from "./components/Index";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/navbar/Navbar";
 import HeroSection from "./components/Hero/HeroSection";
