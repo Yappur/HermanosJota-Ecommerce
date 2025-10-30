ced78f8d2e187b4a20a7c28806f34060d023643a
unknown <mateox463@gmail.com>
2025-10-30 09:57:12 -0300

    Se agrega Login funcional

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -67,8 +67,8 @@ export const AuthProvider = ({ children }) => {
 
 export const useAuth = () => {
   const ctx = useContext(AuthContext);
-  if (!ctx) throw new Error("useAuth must be used within AuthProvider");
+  if (!ctx) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
   return ctx;
 };
-
-export default AuthContext;
